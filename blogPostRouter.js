f5cada4868a0598c5cf208461e0cf09b04bfe8e1
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-20 14:27:57 -0700

    Setup blog post router endpoints

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -3,9 +3,43 @@
 const express = require('express');
 const router = express.Router();
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-
+const {BlogPosts} = require('./models');
 console.log('Router is wired up');
+
+BlogPosts.create('lorem ipsum', 'Test content', 'Marty', 'July 4, 2017');
+
+BlogPosts.create('lorem ipsum', 'Test content', 'Bruno', 'August 6, 2017');
+console.log(BlogPosts);
+
+
+//Get request
+router.get('/', (req, res) => {
+  
+});
+
+//Post request
+router.post('/', jsonParser, (req, res) => {
+
+});
+
+//Put request
+router.put('/:id', jsonParser, (req, res) => {
+
+});
+
+//Delete request
+router.delete('/:id', (req, res) => {
+
+});
+
+
+
+
+
+
 
 
 
