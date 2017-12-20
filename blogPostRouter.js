442c3621620c1d24faae38507f63527288e5c8f1
Dan Rosen <drosen84@gmail.com>
2017-12-20 16:04:06 -0600

    added GET and POST handlers

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -17,12 +17,22 @@ console.log(BlogPosts);
 
 //Get request
 router.get('/', (req, res) => {
-  
+    res.json(BlogPosts.get());
 });
 
 //Post request
 router.post('/', jsonParser, (req, res) => {
-
+    const requiredFields = ['title', 'content', 'author', 'publishDate'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = 'Missing ${field}` in request body';
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(item);
 });
 
 //Put request
