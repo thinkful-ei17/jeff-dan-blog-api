c51d0d9b4879264db0daeb84217a7652918dad08
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-20 15:30:01 -0700

    Added put and delete route handlers

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -17,32 +17,58 @@ console.log(BlogPosts);
 
 //Get request
 router.get('/', (req, res) => {
-    res.json(BlogPosts.get());
+  res.json(BlogPosts.get());
 });
 
 //Post request
 router.post('/', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author', 'publishDate'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = 'Missing ${field}` in request body';
-            console.error(message);
-            return res.status(400).send(message);
-        }
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = 'Missing ${field}` in request body';
+      console.error(message);
+      return res.status(400).send(message);
     }
-    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-    res.status(201).json(item);
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
 });
 
 //Put request
 router.put('/:id', jsonParser, (req, res) => {
-
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (`Request path id (${req.params.id}) and request body id '
+      '(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  //Needs to update blog post, currently 500 error.
+  const updatedPost = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
 });
 
 //Delete request
 router.delete('/:id', (req, res) => {
-
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted Blog Post item \`${req.params.ID}\``);
+  res.status(204).end();
 });
 
 
